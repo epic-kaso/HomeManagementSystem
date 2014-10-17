5777257fa8bd2697ce4fb92495fe9255738e656d
Akachukwu Okafor <kasoprecede47@gmail.com>
2014-10-17 23:51:45 +0100

    Some changes and bug fix

--- src/home/manager/system/Bootstrap.java
+++ src/home/manager/system/Bootstrap.java
@@ -1,9 +1,6 @@
 package home.manager.system;
 
-import home.manager.system.common.Communicable;
-import home.manager.system.common.Message;
 import home.manager.system.hardware.SystemHardWare;
-import home.manager.system.hardware.SystemHardWareMessage;
 import home.manager.system.servercomms.ServerCommunication;
 import home.manager.system.utils.Settings;
 
@@ -13,11 +10,11 @@ import java.util.Scanner;
 
 
 /**
- *
  * **********************************************************************
- * @author  :  OKAFOR AKACHUKWU
- * @email   :  kasoprecede47@gmail.com
- * @date    :  10/16/2014
+ *
+ * @author :  OKAFOR AKACHUKWU
+ * @email :  kasoprecede47@gmail.com
+ * @date :  10/16/2014
  * This file was created by the said author as written above
  * see http://www.kaso.co/
  * **********************************************************************
@@ -27,15 +24,14 @@ import java.util.Scanner;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 public class Bootstrap {
 
@@ -75,15 +71,6 @@ public class Bootstrap {
 
             ServerCommunication.initialize(username, password);
             SystemHardWare.initialize();
-
-            Communicable c = SystemHardWare.getInstance();
-
-            Message m = new SystemHardWareMessage();
-            m.setModuleId(4);
-            m.setMessage("1");
-
-            c.sendMessage(m);
-
 
             homeManagerSystem.manage(ServerCommunication.getInstance(),
                     SystemHardWare.getInstance());
--- src/home/manager/system/HomeManagerSystem.java
+++ src/home/manager/system/HomeManagerSystem.java
@@ -1,8 +1,8 @@
 package home.manager.system;
 
 import home.manager.system.common.Communicable;
-import home.manager.system.common.Listener;
 import home.manager.system.common.Message;
+import home.manager.system.hardware.SystemHardWareMessage;
 
 /**
  * **********************************************************************
@@ -39,20 +39,20 @@ public class HomeManagerSystem {
                        final Communicable systemHardWare) {
 
 
-        serverCommunication.addListener(new Listener() {
-            @Override
-            public void executeMessage(Communicable communicable, Message message) {
-                systemHardWare.sendMessage(message);
-            }
-        });
+        serverCommunication.addListener((communicable, message) -> systemHardWare.sendMessage(message));
 
 
-        systemHardWare.addListener(new Listener() {
-            @Override
-            public void executeMessage(Communicable communicable, Message message) {
-                serverCommunication.sendMessage(message);
-            }
-        });
+        systemHardWare.addListener((communicable, message) -> serverCommunication.sendMessage(message));
 
+        testMessaging(systemHardWare);
+
+    }
+
+    private void testMessaging(Communicable c) {
+        Message m = new SystemHardWareMessage();
+        m.setModuleId(4);
+        m.setMessage("1");
+
+        c.sendMessage(m);
     }
 }
